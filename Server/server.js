26298780ddfe70f259d10e140e5c4582914f69f3
Ayush Singh <202051042@iiitvadodara.ac.in>
2023-05-11 21:55:56 +0530

    first commit

---
+++ Server/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import Connection from './database/db.js';
+import DefaultData from './default.js';
+import dotenv from 'dotenv';
+import router from './routes/route.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import {v4 as uuid} from 'uuid';
+
+const app=express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/',router);
+
+const USERNAME=process.env.DB_USERNAME;
+const PASSWORD=process.env.DB_PASSWORD;
+
+
+
+const PORT=process.env.PORT || 8000;
+
+const URL=process.env.MONGODB_URI || `mongodb://${USERNAME}:${PASSWORD}@ecommerce-shard-00-00.c1gvx.mongodb.net:27017,ecommerce-shard-00-01.c1gvx.mongodb.net:27017,ecommerce-shard-00-02.c1gvx.mongodb.net:27017/?ssl=true&replicaSet=atlas-o5gy3k-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+Connection(URL);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+}
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+});
+
+// DefaultData();
+
+export let paytmMerchantkey=process.env.PAYTM_MERCHANT_KEY;
+export let paytmParams={};
+paytmParams['MID']=process.env.PAYTM_MID;
+paytmParams['WEBSITE']=process.env.PAYTM_WEBSITE;
+paytmParams['CHANNEL_ID']=process.env.PAYTM_CHANNEL_ID;
+paytmParams['INDUSTRY_TYPE_ID']=process.env.PAYTM_INDUSTRY_TYPE_ID;
+paytmParams['ORDER_ID']=uuid();
+paytmParams['CUST_ID']=process.env.PAYTM_CUST_ID;
+paytmParams['TXN_AMOUNT']='100';
+paytmParams['CALLBACK_URL']='/callback';
+paytmParams['EMAIL']='ayusgsingh796@gmail.com';
+paytmParams['MOBILE_NO']='1234567852';
+
